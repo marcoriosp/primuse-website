4106b0da969a0fdfa326f2df4db91fd6067d8098
Marco Rios <mrios@primuse.net>
2025-05-20 12:56:06 +0800

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 const montserrat = Montserrat({ 
   subsets: ["latin"],
@@ -22,7 +23,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5TNRBJD8');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${montserrat.variable} font-sans min-h-screen flex flex-col`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{ __html: `
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5TNRBJD8" 
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        `}} />
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
